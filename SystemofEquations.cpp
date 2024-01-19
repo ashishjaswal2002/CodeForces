330d249ea3105b9d4858abef316fd9d8632c97f9
Ashish jaswal <jaswalashish131@gmail.com>
2024-01-20 02:41:19 +0530

    5 Problem done

---
+++ SystemofEquations.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+ //Brute force approach
+   int n,m;
+   cin>>n>>m;
+   
+  int sum = n+m;//Add both so i can find maximum value..
+  int pair =0;
+
+  for(int i=0;i<sum;i++){
+
+    for(int j=0;j<sum;j++){
+
+        if((i*i+j)==n && (i+j*j)==m){
+         pair++;
+
+        }
+    }
+  }
+
+  cout<<pair<<endl;
+
+
+
+
+
+
+}   
