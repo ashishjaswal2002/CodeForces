3d3a7a430bd0454f7c36dd539bb9fd1f14ab97ec
Ashish jaswal <jaswalashish131@gmail.com>
2024-01-25 00:54:38 +0530

    new problem added

---
+++ Watermelon.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+ 
+   int w;
+   cin>>w;
+ 
+
+ if(w<=2){
+
+    cout<<"NO"<<endl;
+ }
+
+ else{
+
+    if(w%2==0){
+        cout<<"YES"<<endl;
+    }else{
+        cout<<"NO"<<endl;
+    }
+
+ }
+
+
+
+   
+}
+
