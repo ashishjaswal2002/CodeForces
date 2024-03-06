9f58dc0f8c41738be2ec7c0f080c522736f661dd
Ashish jaswal <jaswalashish131@gmail.com>
2024-03-07 00:14:29 +0530

    added new problem

---
+++ Waytoolongwords.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+#define TestCase int t; cin>>t; while(t--)
+using namespace std;
+int main(){
+ 
+   int n;
+   cin>>n;
+
+   while(n--){
+    string s;
+    cin>>s;
+
+  if(s.size()>10){
+ 
+   cout<<s[0]+to_string(s.size()-2)+s[s.size()-1]<<"\n";
+  }else{
+    cout<<s<<"\n";
+  }
+
+
+
+   }
+
+
+}
